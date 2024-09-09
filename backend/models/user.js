5a61bd5818a699472ad72df841e8d2015a65cb6b
nimit sodhani <nimitsodhani1@gmail.com>
2024-09-10 01:14:56 +0530

    db

---
+++ backend/models/user.js
@@ -0,0 +1,23 @@
+// user model 
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+// Create Schema
+const UserSchema = new Schema ({ 
+    username: {
+        type: String,
+        required: true
+    },
+    
+    password: {
+        type: String,
+        required: true
+    },
+    
+    email: {
+        type: String,
+        required: true
+    },
+    
+
+} , {timestamps: true});
+module.exports = User = mongoose.model('user', UserSchema);
--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,26 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const app = express();
 app.use(express.json());
 app.get('/' , (req , res)=>{
     res.send('threads madafaka');
 })
+
+// db connect env
+const uri = process.env.URL; 
+mongoose.connect(uri);
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+})
+
+// routes
 const PORT = process.env.PORT || 5000;  
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-}) ; 
+});
